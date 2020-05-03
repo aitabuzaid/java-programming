acb59642acaf976c597a8c1146c181cf43e79295
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-05-03 20:06:47 +0300

    feat: 2nd attempted solution for min bribes

--- hackerrank/MinBribes.java
+++ hackerrank/MinBribes.java
@@ -13,15 +13,26 @@ public class MinBribes {
 	}
 
 	static void minBribes(int[] q) {
-		int count = 0;
-		for (int i = 0; i < q.length; i++) {
-			if (q[i] > i+3) {
-				System.out.println("Too chaotic");
-				return;
-			}
-			else if (q[i] > i+1)
-				count += q[i]-i-1;                
-		}
-		System.out.println(count);
-	}
+        int count = 0;
+        int temp = -1;
+        for (int i = 0; i < q.length; i++) {
+            if (q[i] > i+3) {
+                System.out.println("Too chaotic");
+                return;
+            }
+            else if (q[i] > i+1)
+                count += q[i]-i-1;                
+            else if (q[i] < i+1){
+                if (temp == -1)
+                    temp = q[i];
+                else {
+                    if (temp < q[i])
+                        temp = q[i];
+                    else
+                        count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
 }
