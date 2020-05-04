0bb2060cf5480062cfd80603ae3895c7715e7f86
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-05-04 23:47:22 +0300

    feat: completed 3rd attempt for min bribes

--- hackerrank/MinBribes.java
+++ hackerrank/MinBribes.java
@@ -8,22 +8,37 @@ public class MinBribes {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] q = {3,1,2,4,5,6};
-		minBribes(q);
+		minimumBribes(q);
 
 	}
 
 	static void minimumBribes(int[] q) {
 		int count = 0;
 		int max = q.length-1;
-		while(max != 0) {
-			int i = max;
-			while(q[i] <= i+1)
+		int temp;
+		int moves;
+		int i;
+		while(max >= 0) {
+			i = max;
+			while((i > 0) && (q[i] <= i+1))
 				i--;
-			while(q[i] != i+1){
-
+			moves = 0;
+			while((i <= max) && (q[i] != i+1)){
+				if (moves == 2) {
+					System.out.println("Too chaotic");
+					return;
+				}
+				temp = q[i+1];
+				q[i+1] = q[i];
+				q[i] = temp;
+				count++;
+				i++;
+				moves++;
 			}
+			if (q[i] == i+1) max = i-1;
+
 		}
-		System.out.println("Too chaotic");
+
 		System.out.println(count);
 	}
 }
