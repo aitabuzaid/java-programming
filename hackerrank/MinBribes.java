0f08fa0f2df976e90304b9c506801032923510b1
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-05-06 23:20:54 +0300

    feat: completed 5th attempt for min bribes - more optimized using hashmaps

--- hackerrank/MinBribes.java
+++ hackerrank/MinBribes.java
@@ -1,4 +1,5 @@
 package hackerrank;
+import java.util.*;
 
 public class MinBribes {
 
@@ -8,26 +9,31 @@ public class MinBribes {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//int[] q = {3,1,2,4,5,6};
-		int[] q = {1,2,5,3,7,8,6,4};
+		//int[] q = {1,2,5,3,7,8,6,4};
+		//int[] q = {3,4,5,2,1};
+		int[] q = {3,4,2,1,7,8,6,5};
 		minimumBribes(q);
 
 	}
 
-    static void minimumBribes(int[] q) {
-        int count = 0;
-        int temp = -1;
-        for (int i = 0; i < q.length; i++) {
-            if (q[i] > i+3) {
-                System.out.println("Too chaotic");
-                return;
-            }
-            else if (q[i] > i+1)
-                count += q[i]-i-1;                
-            else if (q[i] <= i+1){
-                if (temp > q[i]) count++;
-                temp = q[i];
-            }
-        }
+	static void minimumBribes(int[] q) {
+		HashMap<Integer, Integer> dict = new HashMap<Integer,Integer>();
+		int count = 0;
+		for (int i = 0; i < q.length; i++) {
+			dict.put(q[i], 0);
+			for (int key :dict.keySet()){
+				if (q[i] < key) {
+					if (dict.get(key)+1 > 2) {
+						System.out.println("Too chaotic");
+						return;
+					}
+					else{
+						dict.put(key, dict.get(key)+1);
+						count++;
+					}
+				}
+			}
+		}
         System.out.println(count);
     }
 }
