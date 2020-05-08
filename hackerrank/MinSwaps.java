e3460170efd931459892f4539a26b5845a0a4c1b
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-05-08 16:46:24 +0300

    feat: initial commit for min swaps - skeleton code

---
+++ hackerrank/MinSwaps.java
@@ -0,0 +1,21 @@
+package hackerrank;
+import java.util.*;
+import java.util.Map.Entry;
+
+public class MinSwaps {
+
+	// Complete the minimumBribes function below.
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {7,1,3,2,4,5,6};
+		minimumSwaps(arr);
+
+	}
+
+	
+	static void minimumSwaps(int[] arr) {
+	
+	}		
+}
