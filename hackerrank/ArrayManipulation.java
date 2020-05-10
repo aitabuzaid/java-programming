9cb7922a5c53ad93ca59e534921b20225b895392
Abdulrahman Abuzaid <abdulrahman.abuzaid.1@gmail.com>
2020-05-10 22:42:54 +0300

    feat: initial commit for array manipulation

---
+++ hackerrank/ArrayManipulation.java
@@ -0,0 +1,24 @@
+package hackerrank;
+import java.util.*;
+import java.util.Map.Entry;
+
+public class ArrayManipulation {
+
+	// Complete the ArrayManipulation function below.
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr = {{7,1},{2,3}};
+		System.out.println(arrayManipulation(10,arr));
+
+	}
+
+	static long arrayManipulation(int n, int[][] queries) {
+		long res = 1;
+		return res;
+
+	}	
+
+
+}
