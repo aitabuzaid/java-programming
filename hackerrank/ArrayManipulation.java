132224dd5e897d3aae65cdf3b738b008d26df6bc
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-05-11 17:48:34 +0300

    feat: completed 1st attempt for array manipulation

--- hackerrank/ArrayManipulation.java
+++ hackerrank/ArrayManipulation.java
@@ -8,16 +8,23 @@ public class ArrayManipulation {
 
 
 	public static void main(String[] args) {
-		
-		int[][] arr = {{7,1},{2,3}};
-		System.out.println(arrayManipulation(10,arr));
+		int n = 5;
+		int[][] arr = {{1,2,100},{2,5,100},{3,4,100}};
+		System.out.println(arrayManipulation(n,arr));
 
 	}
 
 	static long arrayManipulation(int n, int[][] queries) {
-		long res = 1;
-		return res;
-	}	
+        long max = 0;
+        int[] arr = new int[n]; 
+        for (int i = 0; i < queries.length; i++){
+            for (int j = queries[i][0]-1; j <= queries[i][1]-1; j++){
+                arr[j] += queries[i][2];
+                if (arr[j] > max) max = arr[j];
+            }
+        }
+    return max;
+    }
 
 
 }
