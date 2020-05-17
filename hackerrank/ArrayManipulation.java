a032fbdd64835c6cbe592f72482a1b1194dd018a
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-05-17 22:44:13 +0300

    fix: changed array to long type, correct solution but inefficient

--- hackerrank/ArrayManipulation.java
+++ hackerrank/ArrayManipulation.java
@@ -26,7 +26,7 @@ public class ArrayManipulation {
 
 	static long arrayManipulation(int n, int[][] queries) {
         long max = 0;
-        int[] arr = new int[n]; 
+        long[] arr = new long[n]; 
         for (int i = 0; i < queries.length; i++){
             for (int j = queries[i][0]-1; j <= queries[i][1]-1; j++){
                 arr[j] += queries[i][2];
