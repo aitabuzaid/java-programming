5737bfa4e4fb7b5dd5a847911d0de6aef94678a8
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-05-19 19:47:27 +0300

    feat: added test case

--- hackerrank/RotLeft.java
+++ hackerrank/RotLeft.java
@@ -4,11 +4,14 @@ import java.util.Map.Entry;
 
 public class RotLeft {
 
-	// Complete the minimumBribes function below.
+	// Completed the rotate left function
 
 
 	public static void main(String[] args) {
-
+		int[] input = {1,2,3,4,5};
+		int d = 4;
+		int[] output = rotLeft(input, d);
+		print(output);
 	}
 
 	
@@ -18,6 +21,13 @@ public class RotLeft {
             result[i] = a[(i+d)%a.length];
         }
         return result;
+    }
+    
+    static void print(int[] a) {
+    	for (int i = 0; i < a.length; i++) {
+    		System.out.print(a[i]+" ");
+    	}
+    	System.out.println();
     }
 		
 }
