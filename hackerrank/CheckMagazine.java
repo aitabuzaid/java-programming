e50071b72ad62c9252d432f831905351763e2353
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-05-22 16:18:10 +0300

    feat: skeleton for checkMagazine

---
+++ hackerrank/CheckMagazine.java
@@ -0,0 +1,14 @@
+package hackerrank;
+
+public class CheckMagazine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	static void checkMagazine(String[] magazine, String[] note) {
+
+
+    }
+}
