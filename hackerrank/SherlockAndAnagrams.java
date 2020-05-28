d8a288ff535a5f81e2f58ff9705fc2ed5cc49183
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-05-28 21:57:54 +0300

    feat: extracted all substrings

--- hackerrank/SherlockAndAnagrams.java
+++ hackerrank/SherlockAndAnagrams.java
@@ -12,14 +12,18 @@ public class SherlockAndAnagrams {
 
     // Complete the sherlockAndAnagrams function below.
     static int sherlockAndAnagrams(String s) {
-
+    	for(int len = 1; len <= s.length(); len++) {
+    		for(int i = 0; i < s.length()-len+1; i++) {
+    			System.out.println(s.substring(i,i+len));
+    		}
+    	}
     	return 0;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int q = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
@@ -29,11 +33,11 @@ public class SherlockAndAnagrams {
 
             int result = sherlockAndAnagrams(s);
 
-            bufferedWriter.write(String.valueOf(result));
-            bufferedWriter.newLine();
+            //bufferedWriter.write(String.valueOf(result));
+            //bufferedWriter.newLine();
         }
 
-        bufferedWriter.close();
+        //bufferedWriter.close();
 
         scanner.close();
     }
