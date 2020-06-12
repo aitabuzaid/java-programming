32c7b6ee99dbb9f9a217355c7a9461720604ac27
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-06-12 14:40:32 +0300

    feat: initial commit for count triplets challenge

---
+++ hackerrank/CountTriplets.java
@@ -0,0 +1,33 @@
+package hackerrank;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class CountTriplets {
+
+    // Complete the countTriplets function below.
+    static long countTriplets(List<Long> arr, long r) {
+
+    	return 0;
+    }
+
+    public static void main(String[] args) {
+    	List<Long> list = new ArrayList<Long>();
+    	list.add((long)1);
+    	list.add((long)3);
+    	list.add((long)3);
+    	list.add((long)9);
+    	list.add((long)27);
+    	System.out.println(list);
+        System.out.println(countTriplets(list,3));
+        
+    }
+}
