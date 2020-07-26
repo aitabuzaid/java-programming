f3172433b1b7ce1d7fd8792a3753068e6c9a1471
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-07-26 22:25:51 +0300

    feat: 1st attempt for fraud activity challenge

--- hackerrank/Fraud.java
+++ hackerrank/Fraud.java
@@ -12,7 +12,27 @@ public class Fraud {
 
     // Complete the activityNotifications function below.
     static int activityNotifications(int[] expenditure, int d) {
-    	return 0;
+    	//LinkedList<Integer> list = new LinkedList<Integer>();
+    	int[] arr = new int[d];
+    	int[] res = new int[d];
+    	int count = 0;
+    	for (int i = 0; i < expenditure.length; i++) {
+    		if (i < d)
+    			arr[i%d] = expenditure[i];
+    		else {
+    			res = arr.clone();
+        		Arrays.sort(res);
+        		float med;
+        		if (d % 2 == 0)
+        			med = (float)(res[d/2]+res[d/2-1])/2;
+        		else
+        			med = res[d/2];
+        		if (expenditure[i] >= 2*med) 
+        			count++;
+        		arr[i%d] = expenditure[i];    				
+    		}
+    	}
+    	return count;
     }
 
 
