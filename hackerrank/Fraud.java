9d86a45263b44e81aa9c4929512afd68e59d54ff
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-07-26 20:20:38 +0300

    feat: initial commit for fraud activity challenge

---
+++ hackerrank/Fraud.java
@@ -0,0 +1,25 @@
+package hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Fraud {
+
+    // Complete the activityNotifications function below.
+    static int activityNotifications(int[] expenditure, int d) {
+    	return 0;
+    }
+
+
+    public static void main(String[] args)  {
+    	int[] exp = {1,3,2,5,1,2,4,6,10,3,2,9};
+    	int d = 3;
+    	System.out.println(activityNotifications(exp, d));
+    }
+}
+
