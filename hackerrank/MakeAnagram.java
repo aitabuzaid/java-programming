7c2bc58cf22d7989364d84b66b7276a6e684ccc7
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-07-30 18:05:42 +0300

    feat: initial commit for makeAnagrams challenge

---
+++ hackerrank/MakeAnagram.java
@@ -0,0 +1,17 @@
+package hackerrank;
+
+public class MakeAnagram {
+
+	
+    // Complete the makeAnagram function below.
+    static int makeAnagram(String a, String b) {
+
+
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
