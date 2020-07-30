c8525496044755a17e9863968a7e2032ce68f437
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-07-30 18:23:28 +0300

    feat: constructed two hashmaps from strings

--- hackerrank/MakeAnagram.java
+++ hackerrank/MakeAnagram.java
@@ -1,11 +1,33 @@
 package hackerrank;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class MakeAnagram {
 
 	
     // Complete the makeAnagram function below.
     static int makeAnagram(String a, String b) {
-
+    	HashMap<Character,Integer> mapA = new HashMap<Character,Integer>(); 
+    	for (char ch: a.toCharArray()) 	{
+    		if (!mapA.containsKey(ch))
+    			mapA.put(ch, 1);
+    		else
+    			mapA.put(ch, mapA.get(ch)+1);
+    	}
+    	
+    	HashMap<Character,Integer> mapB = new HashMap<Character,Integer>(); 
+    	for (char ch: b.toCharArray()) 	{
+    		if (!mapB.containsKey(ch))
+    			mapB.put(ch, 1);
+    		else
+    			mapB.put(ch, mapB.get(ch)+1);
+    	}
 
     }
     
