c8d6768b936eafec906cc7925c2ef7462681011d
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-07-31 08:25:48 +0300

    feat: completed MakeAnagrams challenge

--- hackerrank/MakeAnagram.java
+++ hackerrank/MakeAnagram.java
@@ -14,26 +14,50 @@ public class MakeAnagram {
     // Complete the makeAnagram function below.
     static int makeAnagram(String a, String b) {
     	HashMap<Character,Integer> mapA = new HashMap<Character,Integer>(); 
+    	HashMap<Character,Integer> mapB = new HashMap<Character,Integer>(); 
+    	Set<Character> checkedSet = new HashSet<Character>();
+    	int count = 0;
+    	
     	for (char ch: a.toCharArray()) 	{
     		if (!mapA.containsKey(ch))
     			mapA.put(ch, 1);
     		else
     			mapA.put(ch, mapA.get(ch)+1);
     	}
-    	
-    	HashMap<Character,Integer> mapB = new HashMap<Character,Integer>(); 
+
     	for (char ch: b.toCharArray()) 	{
     		if (!mapB.containsKey(ch))
     			mapB.put(ch, 1);
     		else
     			mapB.put(ch, mapB.get(ch)+1);
     	}
+    	
+    	for (char ch: mapA.keySet()) {
+    		if (!checkedSet.contains(ch)) {
+    			checkedSet.add(ch);
+    			if(!mapB.containsKey(ch))
+    				count += mapA.get(ch);
+    			else {
+    				count += Math.abs(mapA.get(ch)-mapB.get(ch));
+    				mapB.remove(ch);
+    			}
+    				
+    		}
+    	}
+    	
+    	for (char ch: mapB.keySet()) {
+    		count += mapB.get(ch);
+    	}
+    	
+    	return count;
+    		
+    	
 
     }
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(makeAnagram("cde", "abc"));
 	}
 
 }
