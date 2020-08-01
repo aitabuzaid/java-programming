922435f9434233af27d046e5e9386efca8afd6a6
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-08-01 10:09:47 +0300

    feat: initial commit for alternating characters challenge

---
+++ hackerrank/AltChar.java
@@ -0,0 +1,23 @@
+package hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class AltChar {
+
+    // Complete the alternatingCharacters function below.
+    static int alternatingCharacters(String s) {
+    	return 0;
+
+    }
+
+
+    public static void main(String[] args) {
+    	System.out.println(alternatingCharacters("AABBAA"));
+    }
+}
