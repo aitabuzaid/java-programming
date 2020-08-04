5c8269e4374d64bad1e6ac1e463c65f25fc6a2ae
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-08-04 22:23:40 +0300

    feat: 4th attempt for Sherlock Valid strings

--- hackerrank/Sherlock.java
+++ hackerrank/Sherlock.java
@@ -10,46 +10,53 @@ import java.util.regex.*;
 
 public class Sherlock {
 
-    // Complete the isValid function below.
-    static String isValid(String s) {
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        for (int i = 0; i < s.length(); i++) {
-            if (!map.containsKey(s.charAt(i)))
-            	map.put(s.charAt(i), 1);
-            else
-            	map.put(s.charAt(i), map.get(s.charAt(i))+1);
-        }
-        
-        Integer val = null;
-        boolean flag = false;
-        int counter = 0;
-        for (char ch: map.keySet()) {
-        	counter++;
-        	if(val == null)
-        		val = map.get(ch);
-        	else {
-        		if (val == map.get(ch))
-        			continue;
-        		else if (!flag && (val < map.get(ch))) {
-        			flag = true;
-        			continue;
-        		}
-        		else if (!flag && (val > map.get(ch)) && counter < 3) {
-        			flag = true;
-        			val = map.get(ch);
-        			continue;
-        		}
-        		else
-        			return "NO";
-         	}        		
-        }
-        return "YES";
-        		
-    }
+	// Complete the isValid function below.
+	static String isValid(String s) {
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		for (int i = 0; i < s.length(); i++) {
+			if (!map.containsKey(s.charAt(i)))
+				map.put(s.charAt(i), 1);
+			else
+				map.put(s.charAt(i), map.get(s.charAt(i))+1);
+		}
+
+		Integer val1 = null;
+		Integer val2 = null;
+		Integer val = null;
+		boolean flag = false;
+		int counter = 0;
+		for (char ch: map.keySet()) {
+			counter++;
+			if (counter == 1)
+				val1 = map.get(ch);
+			else if (counter == 2)
+				val2 = map.get(ch);
+			else if (counter == 3) {
+				if (val1 == val2)
+					val = val1;
+				else if ((val1 == map.get(ch)) || (val2 == map.get(ch)))
+					val = map.get(ch);
+				else
+					return "NO";
+			}
+			if (counter >= 3) {
+				if (val == map.get(ch))
+					continue;
+				else if (!flag && (val != map.get(ch))) {
+					flag = true;
+					continue;
+				}
+				else
+					return "NO";
+			}	
+		}
+		return "YES";
+
+	}
 
 
-    public static void main(String[] args){
-        System.out.println(isValid("abcdefghhgfedecba"));
-    }
+	public static void main(String[] args){
+		System.out.println(isValid("abcdfghhgfeeedcba"));
+	}
 }
 
