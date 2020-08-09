ad5385b8203392415db29fec6d4ea37f08019d32
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-08-09 21:59:18 +0300

    feat: 1st attempt completed for substrcount challenge

--- hackerrank/SubStrCount.java
+++ hackerrank/SubStrCount.java
@@ -13,18 +13,31 @@ public class SubStrCount {
 	// Complete the substrCount function below.
 	static long substrCount(int n, String s) {
 		long count = 0;
-		for (int i = 1; i <= n; i++) {
-			for (int j = 0; j <= n-i; j++) {
-				if (isValid(s.substring(j, j+i-1)))
+		for (int i = 1; i <= n; i++)
+			for (int j = 0; j <= n-i; j++)
+				if (isValid(s.substring(j, j+i))) 
 					count++;
-			}
-			
-		}
 		return count;
 	}
-	
+
 	static boolean isValid(String s) {
-		return false;
+		
+		if (s.length() == 1) return true;
+		
+		if (s.length()%2 != 0) {
+			for (int i = 0; i < s.length(); i++) {
+				if (i != s.length()/2)
+					if (s.charAt(i) != s.charAt(0))
+						return false;
+			}
+		}
+		else {
+			for (int i = 0; i < s.length(); i++) {
+				if (s.charAt(i) != s.charAt(0))
+					return false;
+			}
+		}
+		return true;
 	}
 
 
