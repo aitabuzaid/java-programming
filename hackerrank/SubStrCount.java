dc36a4c8a6b40584452554e560a25aaec8d31ce1
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-08-09 19:35:39 +0300

    feat: initial commit for substrcount challenge

---
+++ hackerrank/SubStrCount.java
@@ -0,0 +1,23 @@
+package hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class SubStrCount {
+
+	// Complete the substrCount function below.
+	static long substrCount(int n, String s) {
+		return 1;
+	}
+
+
+	public static void main(String[] args) {
+
+	}
+
+}
