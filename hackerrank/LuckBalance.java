c00717519c9a5a473649b99b1d38284431d4da66
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-08-12 19:14:44 +0300

    feat: initial commit LuckBalance challenge

---
+++ hackerrank/LuckBalance.java
@@ -0,0 +1,24 @@
+package hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class LuckBalance {
+
+    // Complete the luckBalance function below.
+    static int luckBalance(int k, int[][] contests) {
+
+    	return 1;
+    }
+
+
+
+    public static void main(String[] args){
+    	
+    }
+}
