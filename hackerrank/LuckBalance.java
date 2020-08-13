df76572b68c1caf2b0468c4cdd92fd9fef17813e
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-08-13 19:07:24 +0300

    feat: completed LuckBalance challenge

--- hackerrank/LuckBalance.java
+++ hackerrank/LuckBalance.java
@@ -12,13 +12,39 @@ public class LuckBalance {
 
     // Complete the luckBalance function below.
     static int luckBalance(int k, int[][] contests) {
+    	
+    	LinkedList<Integer> list = new LinkedList<Integer>();
+    	int count = 0;
+    	for (int i = 0; i < contests.length; i++) {
+    		if (contests[i][1] == 1)
+    			list.add(contests[i][0]);
+    		else
+    			count += contests[i][0];
+    			
+    	}
+    	
+    	Object [] tempArr = new Object[list.size()];
+    	tempArr = list.toArray();
+    	Arrays.sort(tempArr);
+    	for (int i = 0; i < tempArr.length; i++)
+    		if (i < (tempArr.length-k))
+    			count += -(int)tempArr[i];
+    		else
+    			count += (int)tempArr[i];
 
-    	return 1;
+    	return count;
     }
 
 
 
     public static void main(String[] args){
-    	
+    	System.out.println(
+    		   luckBalance(3,new int[][] {{5,1},
+    									  {2,1},
+    									  {1,1},
+    									  {8,1},
+    									  {10,0},
+    									  {5,0}})
+    		   );
     }
 }
