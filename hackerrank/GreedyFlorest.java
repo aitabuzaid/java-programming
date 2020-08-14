7b4a6d092d2d7c380f9ab076dda13c157c34cee5
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-08-14 13:54:36 +0300

    feat: initial commit Greedy Florest challenge

---
+++ hackerrank/GreedyFlorest.java
@@ -0,0 +1,22 @@
+package hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class GreedyFlorest {
+
+    // Complete the getMinimumCost function below.
+    static int getMinimumCost(int k, int[] c) {
+
+    	return 1;
+    }
+
+
+    public static void main(String[] args){
+    }
+}
