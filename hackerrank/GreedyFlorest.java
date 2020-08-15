48bc83f1867399e7cda7c01c49b21515417ae40a
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-08-15 21:48:56 +0300

    feat: completed Greedy Florest challenge

--- hackerrank/GreedyFlorest.java
+++ hackerrank/GreedyFlorest.java
@@ -12,11 +12,21 @@ public class GreedyFlorest {
 
     // Complete the getMinimumCost function below.
     static int getMinimumCost(int k, int[] c) {
-
-    	return 1;
+    	Arrays.sort(c);
+    	int counter = 0;
+    	int multiplier = 1;
+    	int cost = 0;
+    	for (int i = c.length-1; i >= 0; i--) {
+    		if ((counter != 0) && (counter % k == 0))
+    			multiplier++;
+    		cost += c[i]*multiplier;
+    		counter++;
+    	}
+    	return cost;
     }
 
 
     public static void main(String[] args){
+    	System.out.println(getMinimumCost(2, new int[] {2,5,6}));
     }
 }
