450970407003a7a0cef3f87fbaa4b5935f9cbc55
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-08-16 19:20:13 +0300

    feat: initial commit MaxMin challenge

---
+++ hackerrank/MaxMin.java
@@ -0,0 +1,23 @@
+package hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class MaxMin {
+
+    // Complete the maxMin function below.
+    static int maxMin(int k, int[] arr) {
+
+    	return 1;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(maxMin(3, new int[] {10, 100, 300, 200, 1000, 20, 30}));
+    }
+}
