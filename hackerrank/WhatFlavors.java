15186e7598b685c80f49df9b030e1ccd8345c299
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-08-17 21:59:18 +0300

    feat: initial commit WhatFlavors challenge

---
+++ hackerrank/WhatFlavors.java
@@ -0,0 +1,21 @@
+package hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class WhatFlavors {
+
+    // Complete the whatFlavors function below.
+    static void whatFlavors(int[] cost, int money) {
+    	
+    }
+
+    public static void main(String[] args) {
+    	whatFlavors(new int[] {1,4,5,3,2}, 4);
+    }
+}
