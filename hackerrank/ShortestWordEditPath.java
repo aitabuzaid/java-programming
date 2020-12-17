49aa4c552de4e642c2408b3a39b1d85f73fb7934
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-12-17 21:15:33 +0300

    implementing graph class for ShortestWordEditPath

--- hackerrank/ShortestWordEditPath.java
+++ hackerrank/ShortestWordEditPath.java
@@ -4,9 +4,22 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
+	class Vertex {
+		String str;
+		int dist;
+		Vertex(String str, int dist) {
+			this.str = str;
+			this.dist = dist;
+		}
+	}
+	
+	class Graph {
+		private Map<Vertex, List<Vertex>> adjVertices;
+	}
 
 	static int shortestWordEditPath(String source, String target, String[] words) {
 		// your code goes here
+		DiGra
 		return 0;
 	}
 
