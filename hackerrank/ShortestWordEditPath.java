65eec8503e99e84cbe8515ca790d79184acc04e9
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-12-18 21:17:46 +0300

    implementing methods for graph class - ShortestWordEditPath

--- hackerrank/ShortestWordEditPath.java
+++ hackerrank/ShortestWordEditPath.java
@@ -15,6 +15,15 @@ class Solution {
 	
 	class Graph {
 		private Map<Vertex, List<Vertex>> adjVertices;
+		void addVertex(String str, int dist) {
+			adjVertices.putIfAbsent(new Vertex(str, dist), new ArrayList<>());
+		}
+		
+		void removeVertex(String str, int dist) {
+			Vertex v = new Vertex(str, dist);
+			adjVertices.values().stream().forEach(e -> e.remove(v));
+			adjVertices.remove(new Vertex(str, dist))
+		}
 	}
 
 	static int shortestWordEditPath(String source, String target, String[] words) {
