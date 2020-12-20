b46096b28bcfd09890e745d9c807d21e96106cf6
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-12-20 22:06:43 +0300

    overrride equals for Vertex class - ShortestWordEditPath

--- hackerrank/ShortestWordEditPath.java
+++ hackerrank/ShortestWordEditPath.java
@@ -5,10 +5,23 @@ import java.util.*;
 
 class Vertex {
 	String str;
-	int dist;
-	Vertex(String str, int dist) {
+	//int dist;
+	Vertex(String str) {//, int dist) {
 		this.str = str;
-		this.dist = dist;
+		//this.dist = dist;
+	}
+	
+	@Override
+	public boolean equals(Object that) {
+		if (this == that)
+			return true;
+		
+		if (!(that instanceof Vertex)) {
+			return false;
+		}
+		
+		Vertex v = (Vertex) that;
+		return this.str.equals(v.str);
 	}
 }
 
@@ -17,17 +30,22 @@ class Graph {
 
 	Graph() {}
 
-	void addVertex(String str, int dist) {
+	void addVertex(String str) {//, int dist) {
 		System.out.println(str);
-		System.out.println(dist);
+		//System.out.println(dist);
 		System.out.println(adjVertices);
-		adjVertices.putIfAbsent(new Vertex(str, dist), new ArrayList<>());
+		//adjVertices.putIfAbsent(new Vertex(str, dist), new ArrayList<>());
+		adjVertices.putIfAbsent(new Vertex(str), new ArrayList<>());
 	}
 
-	void addEdge(String str1, int dist1, String str2, int dist2) {
-		Vertex v1 = new Vertex(str1, dist1);
-		Vertex v2 = new Vertex(str2, dist2);
+	//void addEdge(String str1, int dist1, String str2, int dist2) {
+	void addEdge(String str1, String str2) {
+		Vertex v1 = new Vertex(str1);//, dist1);
+		Vertex v2 = new Vertex(str2);//, dist2);
+		
 		System.out.println(adjVertices);
+		System.out.println(adjVertices.get(v1));
+		System.out.println(v1);
 		adjVertices.get(v1).add(v2);
 		adjVertices.get(v2).add(v1);
 	}
@@ -46,12 +64,13 @@ class ShortestWordEditPath {
 			System.out.println("test");
 			String cur = queue.remove();
 			visited.add(cur);
-			graph.addVertex(cur, dist);
+			graph.addVertex(cur);//, dist);
 			for (int i = 0; i < words.length; i++) {
 				if (editDistance(cur, words[i]) == 1 && !visited.contains(words[i])) {
 					queue.add(words[i]);
-					graph.addVertex(words[i], dist+1);
-					graph.addEdge(cur, dist, words[i], dist+1);
+					graph.addVertex(words[i]);//, dist+1);
+					//graph.addEdge(cur, dist, words[i], dist+1);
+					graph.addEdge(cur, words[i]);
 					if (editDistance(words[i], target) == 1) {
 						return dist+2;
 					}
