cdaa33f011ed82d6cde859d0155f2476c694d072
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-12-25 23:02:23 +0300

    completed challenge - ShortestWordEditPath

--- hackerrank/ShortestWordEditPath.java
+++ hackerrank/ShortestWordEditPath.java
@@ -6,7 +6,15 @@ import java.util.*;
 class ShortestWordEditPath {
 	static int shortestWordEditPath(String source, String target, String[] words) {
 		// your code goes here
-		Graph graph = new Graph();
+		boolean missing = true;
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].equals(target)) {
+				missing = false;
+			}
+		}
+		if (missing)
+			return -1;
+			
 		//Queue<String> queue = new LinkedList<String>();
 		Queue<Vertex> queue = new LinkedList<Vertex>();
 		Set<String> visited = new HashSet<String>(); 
@@ -17,16 +25,17 @@ class ShortestWordEditPath {
 
 			//String cur = queue.remove();
 			Vertex cur = queue.remove();
-			System.out.println(cur);
+			//System.out.println(cur);
 			visited.add(cur.str);
-			graph.addVertex(cur.str, dist);
+			//graph.addVertex(cur.str, dist);
 			for (int i = 0; i < words.length; i++) {
 				if (editDistance(cur.str, words[i]) == 1 && !visited.contains(words[i])) {
-
+					System.out.print(cur.str+" ");
+					System.out.println(words[i]);
 					queue.add(new Vertex(words[i], cur.dist+1));
-					graph.addVertex(words[i], cur.dist+1);
+					//graph.addVertex(words[i], cur.dist+1);
 					//graph.addEdge(cur, dist, words[i], dist+1);
-					graph.addEdge(cur.str, cur.dist, words[i], cur.dist+1);
+					//graph.addEdge(cur.str, cur.dist, words[i], cur.dist+1);
 					if (editDistance(words[i], target) == 1) {
 						return cur.dist+2;
 					}
@@ -37,6 +46,8 @@ class ShortestWordEditPath {
 	}
 
 	static int editDistance(String str1, String str2) {
+		if (str1.length() != str2.length())
+			return 0;
 		int dist = 0;
 		for (int i = 0; i < str1.length(); i++) {
 			if (str1.charAt(i) != str2.charAt(i))
@@ -45,16 +56,22 @@ class ShortestWordEditPath {
 		return dist;
 	}
 
-
-
 	public static void main(String[] args) {
+		String source = "no";
+		String target = "go";
+		String[] words = new String[]{"to"};
+		
+		/*
 		String source = "bit";
 		String target = "dog";
 		String[] words = new String[]{"but", "put", "big", "pot", "pog", "dog", "lot"};
+		*/
 		System.out.println(shortestWordEditPath(source, target, words));
 	}
 
+
 }
+
 class Vertex {
 	String str;
 	int dist;
@@ -62,26 +79,16 @@ class Vertex {
 		this.str = str;
 		this.dist = dist;
 	}
-}
+	@Override
+	public boolean equals(Object that) {
+		if (this == that)
+			return true;
 
-class Graph {
-
-	Graph(){}
-
-	
-	private Map<Vertex, List<Vertex>> adjVertices = new HashMap<Vertex, List<Vertex>>();
-
-
-	public void addVertex(String str, int dist) {
-		//System.out.println(adjVertices);
-		adjVertices.putIfAbsent(new Vertex(str, dist), new ArrayList<Vertex>());
-	}
-
-	public void addEdge(String str1, int dist1, String str2, int dist2) {
-		//void addEdge(String str1, String str2) {	
-		Vertex v1 = new Vertex(str1, dist1);
-		Vertex v2 = new Vertex(str1, dist1);
-		adjVertices.get(v1).add(v2);
-		adjVertices.get(v2).add(v1);
+		if (!(that instanceof Vertex)) {
+			return false;
+		}
+		System.out.println("hi");
+		Vertex v = (Vertex) that;
+		return this.str.equals(v.str);
 	}
 }
