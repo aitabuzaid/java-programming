be7ba0a001e8e6be7dcc20b048a481e4fc19b44c
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-12-27 20:46:26 +0300

    feat: added multiplication initial code

---
+++ hackerrank/Multiplication.java
@@ -0,0 +1,18 @@
+package hackerrank;
+
+import java.io.*;
+import java.util.*;
+
+class Multiplication {
+	static String mult(String x, String y) {
+		
+		return new String();
+	}
+
+
+	public static void main(String[] args) {
+		
+	}
+
+
+}
