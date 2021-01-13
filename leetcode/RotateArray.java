8c62036e6840101ac074ed644c9d6a500b12e014
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-01-13 20:23:51 +0300

    feat: 1st attempt rotate array- o(1) space

--- leetcode/RotateArray.java
+++ leetcode/RotateArray.java
@@ -1,17 +1,31 @@
 package leetcode;
 
-class RotateArray {
-    public void rotate(int[] nums, int k) {
-        
-        int[] res = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {      
-            res[i] = nums[i]; 
-        }
-        int j;
-        for (int i = 0; i < nums.length; i++) {
-            j  = ((i+nums.length-k)%nums.length + nums.length)%nums.length;           
-            nums[i] = res[j]; 
 
-        }
-    }
+class RotateArray {
+	public void rotate(int[] nums, int k) {
+		k = k%nums.length;
+		int i = 0;
+		int temp = nums[i];
+		int j = (i-k+nums.length) % nums.length;
+		while (j != 0) {
+			//System.out.println(j);
+			nums[i] = nums[j];
+			i = j;
+			j = (i-k+nums.length) % nums.length;
+		}
+		nums[i] = temp;
+		if (nums.length % 2 == 0 & k % 2 == 0){
+			for (int l = 1; l < 2; l++) {
+				i = l;
+				temp = nums[i];
+				j = (i-k+nums.length) % nums.length;
+				while (j != l) {
+					nums[i] = nums[j];
+					i = j;
+					j = (i-k+nums.length) % nums.length;
+				}
+				nums[i] = temp;
+			}
+		}
+	}
 }
