9ed97473cd54d64ffa9d229f61cfb92ef5f2817d
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-01-14 21:44:16 +0300

    feat: adding test cases for 1st attempt rotate array- o(1) space

--- leetcode/RotateArray.java
+++ leetcode/RotateArray.java
@@ -2,7 +2,7 @@ package leetcode;
 
 
 class RotateArray {
-	public void rotate(int[] nums, int k) {
+	public static void rotate(int[] nums, int k) {
 		k = k%nums.length;
 		int i = 0;
 		int temp = nums[i];
@@ -27,5 +27,15 @@ class RotateArray {
 				nums[i] = temp;
 			}
 		}
+	}
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,5,6,7,8,9};
+		int k = 3;
+		rotate(arr, k);
+		for (int i : arr) {
+			System.out.print(i+" ");
+		}
+		
+		
 	}
 }
