7855307389375387b5ea2ee9d742b5f548029dff
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-01-18 15:59:06 +0300

    feat: completed containsDuplicate

---
+++ leetcode/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import java.util.*;
+
+class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i : nums){
+            if (set.contains(i))
+            	return true;
+            set.add(i);
+        }
+        return false;
+    }
+}
