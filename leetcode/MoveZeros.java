73eacb3203db02b6137c01f9a6516ecb1dee5210
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-01-22 22:51:35 +0300

    feat: 1st attempt move zeroes

--- leetcode/MoveZeros.java
+++ leetcode/MoveZeros.java
@@ -1,7 +1,41 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class MoveZeros {
-    public void moveZeroes(int[] nums) {
+    public static void moveZeroes(int[] nums) {
+        moveZeroes(nums, 0, nums.length-1);
         
+    }
+    
+    public static void moveZeroes(int[] nums, int min, int max) {
+    	if (max <= min)
+    		return;
+    	int mid = (min + max)/2;
+    	moveZeroes(nums, min, mid);
+    	moveZeroes(nums, mid+1, max);
+    	mid++;
+    	while (min <= max) {
+    		if (nums[min] != 0)
+    			min++;
+    		else if (nums[mid] != 0) {
+    			swap(nums, min++, mid++);
+    		}
+    		else
+    			break;
+    			
+    		
+    	}
+    }
+    public static void swap(int[] nums, int i, int j) {
+    	int temp = nums[i];
+    	nums[i] = nums[j];
+    	nums[j] = temp;
+    }
+    
+    public static void main(String[] args) {
+    	int[] arr = new int[] {1,3,2,0,4,9,0,0,5};
+    	moveZeroes(arr);
+    	System.out.println(Arrays.toString(arr));
     }
 }
