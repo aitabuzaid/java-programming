e6a997c90121fa75cf992c675a98962820c599c0
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-01-23 07:54:49 +0300

    feat: 2nd attempt move zeroes- O(n logn)

--- leetcode/MoveZeros.java
+++ leetcode/MoveZeros.java
@@ -9,25 +9,33 @@ public class MoveZeros {
     }
     
     public static void moveZeroes(int[] nums, int min, int max) {
+    	System.out.println(min+" "+ max);
     	if (max <= min)
     		return;
     	int mid = (min + max)/2;
     	moveZeroes(nums, min, mid);
     	moveZeroes(nums, mid+1, max);
     	mid++;
-    	while (min <= max) {
+    	/*
+    	while (nums[min] != 0 && max < )
+    		min++;
+    	System.out.println(min+" "+ max);
+    	while (nums[mid] != 0)
+    		swap(nums, min++, mid++);
+    	*/
+    	
+    	while (min < max) {
     		if (nums[min] != 0)
     			min++;
-    		else if (nums[mid] != 0) {
+    		else if (nums[mid] != 0 && mid <= max) {
     			swap(nums, min++, mid++);
     		}
     		else
     			break;
-    			
-    		
     	}
     }
     public static void swap(int[] nums, int i, int j) {
+    	System.out.println("swap: "+i+" "+ j);
     	int temp = nums[i];
     	nums[i] = nums[j];
     	nums[j] = temp;
