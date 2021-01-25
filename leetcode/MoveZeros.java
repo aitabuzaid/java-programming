7f8b72bafa94d30d1b9d431633d48f2b80b8fc47
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-01-25 20:11:21 +0300

    feat: completed  move zeroes- O(n)

--- leetcode/MoveZeros.java
+++ leetcode/MoveZeros.java
@@ -4,13 +4,15 @@ import java.util.Arrays;
 
 public class MoveZeros {
     public static void moveZeroes(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-        	if (nums[i] == 0) {
-        		int j = i+1;
-        		while (nums[j]) 
+        int j = -1;
+    	for (int i = 0; i < nums.length; i++) {
+        	if (nums[i] == 0 && j == -1) {
+        		j = i; 
+        	}
+        	if (nums[i] != 0 && j > -1) {
+        		swap(nums, i, j++);
         	}
         }
-        
     }
     
 
@@ -22,10 +24,11 @@ public class MoveZeros {
     }
     
     public static void main(String[] args) {
-    	//int[] arr = new int[] {1,3,2,0,4,9,0,0,5};
+    	int[] arr = new int[] {1,3,2,0,4,9,0,0,5};
+    	//int[] arr = new int[] {0,0,0,0,0,0,5};
     	//int[] arr = new int[] {1,3,2,2,4,9,6,7,5};
     	//int[] arr = new int[] {0,1,0,0,0,4,9,0,0,0,5,6,0,0,7};
-    	int[] arr = new int[] {0,0,0,0,0,0,0,9,6,5,9,8,1,5,7};
+    	//int[] arr = new int[] {0,0,0,0,0,0,0,9,6,5,9,8,1,5,7};
     	moveZeroes(arr);
     	System.out.println(Arrays.toString(arr));
     }
