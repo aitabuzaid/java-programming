da7d7eddd7ec0d6566a263688736979578845cf1
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-01-27 21:37:03 +0300

    feat: initial commit for isValidSudoku

---
+++ leetcode/ValidSudoku.java
@@ -0,0 +1,23 @@
+package leetcode;
+import java.util.*;
+
+public class ValidSudoku {
+
+	public boolean isValidSudoku(char[][] board) {
+		HashMap<Character, HashMap<Character, Character>> map =
+				new HashMap<Character, HashMap<Character, Character>>();
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				
+			}
+		}
+		return false;
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
