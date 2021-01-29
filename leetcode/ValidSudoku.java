0ce3f53dff881d240316d6d8787dd3b72e567a6d
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-01-29 08:24:34 +0300

    feat: completed Valid Sudoku

--- leetcode/ValidSudoku.java
+++ leetcode/ValidSudoku.java
@@ -9,26 +9,30 @@ public class ValidSudoku {
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[0].length; j++) {
 				if (board[i][j] != '.') {
-					if (!map.get(i).contains(board[i][j])) {
-						map.put(i, new HashSet<Character>());
+					if (!map.containsKey(i)){
+                        map.put(i, new HashSet<Character>());
+                        map.get(i).add(board[i][j]);
+                    }
+					else if (!map.get(i).contains(board[i][j]))
 						map.get(i).add(board[i][j]);
-					}
-						
 					else
 						return false;
 				
-					if (!map.get(j+9).contains(board[i][j])) {
-						map.put(((Character)(j+9), new HashSet<Character>());
+					if (!map.containsKey(j+9)){
+                        map.put(j+9, new HashSet<Character>());
+                        map.get(j+9).add(board[i][j]);
+                    }
+					else if (!map.get(j+9).contains(board[i][j])) 
 						map.get(j+9).add(board[i][j]);
-					}
-						
 					else
 						return false;
-					
-					if (!map.get(i+3*j+18).contains(board[i][j])) {
 						
-					}
-						map.get(i+3*j+18).add(board[i][j]);
+					if (!map.containsKey(i/3+3*(j/3)+18)){
+                        map.put(i/3+3*(j/3)+18, new HashSet<Character>());
+                        map.get(i/3+3*(j/3)+18).add(board[i][j]);
+                    }
+                    else if (!map.get(i/3+3*(j/3)+18).contains(board[i][j])) 
+						map.get(i/3+3*(j/3)+18).add(board[i][j]);
 					else
 						return false;
 				}
