4deeb27243a443bb81a3a6ee68ae1e09ec1c78a1
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-02-15 09:42:25 +0300

    feat: reducing memory - Valid Sudoku

--- leetcode/ValidSudoku.java
+++ leetcode/ValidSudoku.java
@@ -4,10 +4,10 @@ import java.util.*;
 public class ValidSudoku {
 
 	public boolean isValidSudoku(char[][] board) {
-		HashMap<Integer, HashSet<Character>> map =
-				new HashMap<Integer, HashSet<Character>>();
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[0].length; j++) {
+		HashMap<Character, HashSet<Character>> map =
+				new HashMap<Character, HashSet<Character>>();
+		for (char i = 0; i < board.length; i++) {
+			for (char j = 0; j < board[0].length; j++) {
 				if (board[i][j] != '.') {
 					if (!map.containsKey(i)){
                         map.put(i, new HashSet<Character>());
