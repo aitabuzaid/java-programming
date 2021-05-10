257813f6a14accee7db9bf14ef03b498df3265f3
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-05-10 12:57:15 +0300

    feat: initial loops

--- leetcode/RotateMatrix.java
+++ leetcode/RotateMatrix.java
@@ -5,7 +5,13 @@ public class RotateMatrix {
     public void rotate(int[][] matrix) {
     	int temp;
     	int size = matrix.length;
-    	//for (int k = 0; )
+    	for (int k = 0; k < size/2; k++) {
+    		for (int l = 0; l < size-(2*k); l++) {
+    			for (int m = 0; m < 3; m++) {
+    				
+    			}
+    		}
+    	}
     }
 
 	public static void main(String[] args) {
