f4a45cc5ea530dd9af0dcf2e9f9242a1f486ca3b
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-05-12 16:33:44 +0300

    feat: copy routine - clockwise

--- leetcode/RotateMatrix.java
+++ leetcode/RotateMatrix.java
@@ -4,14 +4,14 @@ public class RotateMatrix {
 
     public void rotate(int[][] matrix) {
     	int temp;
-    	int size = matrix.length;
+    	int size = matrix.length-1;
     	for (int k = 0; k < size/2; k++) {
     		for (int l = 0; l < size-(2*k); l++) {
     			temp = matrix[l][l];
-    			matrix[l][l] = matrix[l][size-l];
-    			matrix[l][size-l] = matrix[size-l][size-l];
-    			matrix[size-l][size-l] = matrix[size-l][l];
-    			matrix[size-l][l] = temp;
+    			matrix[l][l] = matrix[size-l][l];
+    			matrix[size-l][l] = matrix[size-l][size-l];
+    			matrix[size-l][size-l] = matrix[l][size-l];
+    			matrix[l][size-l] = temp;
     			//for (int m = 0; m < 3; m++) {
     			//}
     		}
