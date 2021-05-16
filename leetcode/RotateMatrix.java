a96ed1be5f9fae30ad5b706ae1a527e967b18407
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-05-16 22:01:07 +0300

    feat: works for size 2 and 3

--- leetcode/RotateMatrix.java
+++ leetcode/RotateMatrix.java
@@ -5,7 +5,7 @@ public class RotateMatrix {
 	public static void rotate(int[][] matrix) {
 		int temp;
 		int size = matrix.length-1;
-		for (int k = 0; k < size/2; k++) {
+		for (int k = 0; k < (size+1)/2; k++) {
 			for (int l = 0; l < size-(2*k); l++) {
 				temp = matrix[0][l];
 				matrix[0][l] = matrix[size-l][0];
@@ -31,7 +31,8 @@ public class RotateMatrix {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+		//int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+		int[][] matrix = {{1,2},{3,4}};
 		printMatrix(matrix);
 		rotate(matrix);
 		printMatrix(matrix);
