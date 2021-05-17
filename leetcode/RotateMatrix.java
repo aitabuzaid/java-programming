a33a6098922c0ac6273e94e90f6f515f316035df
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-05-17 10:51:04 +0300

    feat: completed rotate matrix

--- leetcode/RotateMatrix.java
+++ leetcode/RotateMatrix.java
@@ -6,12 +6,12 @@ public class RotateMatrix {
 		int temp;
 		int size = matrix.length-1;
 		for (int k = 0; k < (size+1)/2; k++) {
-			for (int l = 0; l < size-(2*k); l++) {
-				temp = matrix[0][l];
-				matrix[0][l] = matrix[size-l][0];
-				matrix[size-l][0] = matrix[size][size-l];
-				matrix[size][size-l] = matrix[l][size];
-				matrix[l][size] = temp;
+			for (int l = 0; l < (size)-(2*k); l++) {
+				temp = matrix[0+k][l+k];
+				matrix[0+k][l+k] = matrix[size-l-k][0+k];
+				matrix[size-l-k][0+k] = matrix[size-k][size-l-k];
+				matrix[size-k][size-l-k] = matrix[l+k][size-k];
+				matrix[l+k][size-k] = temp;
 				//for (int m = 0; m < 3; m++) {
 				//}
 			}
@@ -32,7 +32,9 @@ public class RotateMatrix {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
-		int[][] matrix = {{1,2},{3,4}};
+		//int[][] matrix = {{1,2},{3,4}};
+		//int[][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+		int[][] matrix = {{1,2,3,4,5},{5,6,7,8,9},{9,10,11,12,13},{13,14,15,16,17},{17,18,19,20,21}};
 		printMatrix(matrix);
 		rotate(matrix);
 		printMatrix(matrix);
