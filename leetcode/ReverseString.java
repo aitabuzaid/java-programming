e33cc4fc8e7114914c1ca6d49f56510df3ecf40b
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-05-19 22:03:12 +0300

    feat: completed reverse string

--- leetcode/ReverseString.java
+++ leetcode/ReverseString.java
@@ -1,12 +1,24 @@
 package leetcode;
 
 public class ReverseString {
-	public void reverseString(char[] s) {
-
+	public static void reverseString(char[] s) {
+		char temp;
+		for (int i = 0; i < s.length; i++) {
+			if (i >= s.length-i-1)
+				break;
+			temp = s[i];
+			s[i] = s[s.length-i-1];
+			s[s.length-i-1] = temp;
+		}
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//char[] string = "Hello".toCharArray();
+		char[] string = "Hi".toCharArray();
+		System.out.println(string);
+		reverseString(string);
+		System.out.println(string);
 
 	}
 
