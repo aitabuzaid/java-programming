f20694c24cf03567cfdc91e7788c79a0778119f1
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-05-21 20:11:49 +0300

    feat: added test case

--- leetcode/ReverseInt.java
+++ leetcode/ReverseInt.java
@@ -3,11 +3,12 @@ package leetcode;
 public class ReverseInt {
 
 	public static int reverse(int x) {
-
+		
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int test = 1233410;
+		System.out.println(reverse(test));
 	}
 
 }
