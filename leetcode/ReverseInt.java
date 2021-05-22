5d24fcbe7bab906b1d5bb2ec534d283471583159
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-05-22 20:31:17 +0300

    feat: completed challenge

--- leetcode/ReverseInt.java
+++ leetcode/ReverseInt.java
@@ -3,11 +3,27 @@ package leetcode;
 public class ReverseInt {
 
 	public static int reverse(int x) {
+		boolean neg = false;
+		int res = 0, l;
+		if (x < 0) {
+			x = -1*x;
+			neg = true;
+		}
+			
+		while (x > 0) {
+			l = x % 10;
+			res = res*10 + l;
+			x = x / 10;
+		}
 		
+		if (neg)
+			return -res;
+		else
+			return res;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int test = 1233410;
+		int test = -1233410;
 		System.out.println(reverse(test));
 	}
 
