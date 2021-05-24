dc8f4f04bc6d478a93a48fdba302d9fb51267910
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-05-24 20:27:22 +0300

    feat: passed all cases

--- leetcode/ReverseInt.java
+++ leetcode/ReverseInt.java
@@ -12,7 +12,8 @@ public class ReverseInt {
 			
 		while (x > 0) {
 			l = x % 10;
-			if (res*10+l < res)
+			if ((neg && res > (Math.pow(2, 31)-l)/10) ||
+				(!neg && res > (Math.pow(2, 31)-l-1)/10))
 				return 0;
 			res = res*10 + l;
 			x = x / 10;
@@ -26,7 +27,8 @@ public class ReverseInt {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int test = 1534236469;//1233410;
+		//int test = 1534236469;//1233410;
+		int test = 1563847412;
 		System.out.println(reverse(test));
 	}
 
