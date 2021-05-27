9ec87449ac9a12a3d74a8f8ebebb19fe063ddee2
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-05-27 22:34:04 +0300

    feat: completed FirstUniquChar

--- leetcode/FirstUniquChar.java
+++ leetcode/FirstUniquChar.java
@@ -3,31 +3,38 @@ import java.util.*;
 
 public class FirstUniquChar {
 	
+	
 	public static int firstUniqChar(String s) {
-		Hashtable<Character, Integer> table = new Hashtable<Character, Integer>();
+		HashMap<Character, Integer> table = new HashMap<Character, Integer>();
 		for (int i = 0; i < s.length(); i++) {
-			if (!table.contains(s.charAt(i))) {
+			if (!table.containsKey(s.charAt(i))) {
 				table.put(s.charAt(i), i);
 			}
 			else {
-				table.remove(s.charAt(i));
+				table.replace(s.charAt(i), -1);
 			}
 		}
-		char minChar;
+
 		int minInd = (int)Math.pow(2, 31);
+		int ind;
 		for (Character key : table.keySet()){
-			if (table.get(key) < minInd) {
-				minChar = key;
-				minInd = table.get(key);
+			ind = table.get(key);
+			if (ind < minInd && ind != -1) {
+				minInd = ind;
 			}
 				
 		}
+		
+		if (minInd == (int)Math.pow(2, 31))
+			return -1;
 				
 		return minInd;
 	}
-
-	static void main(String args[]) {
-
+	
+	public static void main(String args[]) {
+		String str = "loveleetcode";
+		System.out.println(firstUniqChar(str));
 	}
+	
 
 }
