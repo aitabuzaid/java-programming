ae9baad46ff61123ba06d91f1ce40786514059c1
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-05-29 13:51:35 +0300

    feat: 1st attempt Palindrome

--- leetcode/Palindrome.java
+++ leetcode/Palindrome.java
@@ -2,6 +2,25 @@ package leetcode;
 
 public class Palindrome {
 
+	public boolean isPalindrome(String s) {
+		int i = 0, j = s.length()-1;
+		
+		while (i < j) {
+			while (s.charAt(j) < 'a' || s.charAt(j) > 'z') {
+				j--;
+			}
+			while (s.charAt(i) < 'a' || s.charAt(i) > 'z') {
+				i++;
+			}
+			if (i < j && s.charAt(i) != t.charAt(j))
+				return false;
+
+				
+		}
+		return true;
+		
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
