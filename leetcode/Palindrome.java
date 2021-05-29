4d592d6ca27333bce65597f000a068ea6a725a59
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-05-29 15:02:55 +0300

    feat: 2nd attempt Palindrome

--- leetcode/Palindrome.java
+++ leetcode/Palindrome.java
@@ -2,19 +2,23 @@ package leetcode;
 
 public class Palindrome {
 
-	public boolean isPalindrome(String s) {
+	public static boolean isPalindrome(String s) {
+		s = s.toLowerCase();
 		int i = 0, j = s.length()-1;
 		
 		while (i < j) {
-			while (s.charAt(j) < 'a' || s.charAt(j) > 'z') {
+			while ((j >= 0) && 
+					!((s.charAt(j) >= 'a' && s.charAt(j) <= 'z' ) || (s.charAt(j) >= '0' && s.charAt(j) <= '9'))) {
 				j--;
 			}
-			while (s.charAt(i) < 'a' || s.charAt(i) > 'z') {
+			while ((i < s.length()) &&
+					!((s.charAt(i) >= 'a' && s.charAt(i) <= 'z' ) || (s.charAt(i) >= '0' && s.charAt(i) <= '9'))) {
 				i++;
 			}
-			if (i < j && s.charAt(i) != t.charAt(j))
+			if (i < j && s.charAt(i) != s.charAt(j))
 				return false;
-
+			i++;
+			j--;
 				
 		}
 		return true;
@@ -23,7 +27,10 @@ public class Palindrome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+		//System.out.println(isPalindrome("P"));
+		
 	}
+	
 
 }
