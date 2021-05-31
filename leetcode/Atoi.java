d26788bb6e8351bcfed976ded79e352b8ea59756
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-05-31 21:06:50 +0300

    feat: completed Atoi - 2nd attemp

--- leetcode/Atoi.java
+++ leetcode/Atoi.java
@@ -4,21 +4,51 @@ public class Atoi {
 
 	public static int myAtoi(String s) {
 		int result = 0;
-		boolean sign = true;
+		Boolean sign = null;
+		int l;
 		for (int i  = 0; i < s.length(); i++) {
-			if (s.charAt(i) == '+')
+			if (s.charAt(i) == '+') {
+				if (sign != null)
+					break;
+				
+				sign = true;
 				continue;
+			}
+				
 			if (s.charAt(i) == '-') {
+				if (sign != null)
+					break;
+				
 				sign = false;
 				continue;
 			}
-			if (s.charAt(i) == ' ')
-				continue;
+			if (s.charAt(i) == ' ') {
+				if (sign != null)
+					break;
+				else
+					continue;
+			}
+				
 			if(!Character.isDigit(s.charAt(i)))
-				continue;
-			result = 10*result+(int)(s.charAt(i)-'0');
+				break;
+			
+			if (sign == null)
+				sign = true;
+			
+			l = (int)(s.charAt(i)-'0');
+			
+			if (sign && result > (Math.pow(2, 31)-l-1)/10)
+				return (int)(Math.pow(2, 31));
+			if (!sign && result > (Math.pow(2, 31)-l)/10)
+				return -(int)(Math.pow(2, 31))-1;
+			
+			result = 10*result+l;
 			
 		}
+		
+		if (sign == null)
+			sign = true;
+		
 		if (sign)
 			return result;
 		else
@@ -27,7 +57,7 @@ public class Atoi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(myAtoi("  -1232311353"));
+		System.out.println(myAtoi("   0 123"));
 	}
 
 }
