6e67140a7840ea8165e9feb434450e2cd5b0dede
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-06-03 21:29:38 +0300

    feat: initial commit LongestCommonPrefix

---
+++ leetcode/LongestCommonPrefix.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class LongestCommonPrefix {
+	
+	public static String longestCommonPrefix(String[] strs) {
+        return "";
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
