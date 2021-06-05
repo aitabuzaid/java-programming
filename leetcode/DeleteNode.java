c133afba99f012c92b415e246c7fa3935eee4235
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-06-05 22:19:25 +0300

    feat: completed DeleteNode

---
+++ leetcode/DeleteNode.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class DeleteNode {
+
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
+
+	public static void deleteNode(ListNode node) {
+		node.val = node.next.val;
+		node.next = node.next.next;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+
+
+
+class Solution {
+
+}
