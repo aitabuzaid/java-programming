c70187a124933803a7b334c76c21c6b00264dce3
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-06-24 21:15:34 +0300

    feat: initial commit FizzBuzz

---
+++ leetcode/FizzBuzz.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class FizzBuzz {
+
+	public List<String> fizzBuzz(int n) {
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
