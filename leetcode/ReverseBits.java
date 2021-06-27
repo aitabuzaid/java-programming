e8f6697d0c0006240731427219667b9e7c8c913c
Abdulrahman Abuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2021-06-27 21:15:39 +0300

    feat: completed ReverseBits

--- leetcode/ReverseBits.java
+++ leetcode/ReverseBits.java
@@ -3,8 +3,13 @@ package leetcode;
 public class ReverseBits {
 
 	public int reverseBits(int n) {
-
-	}
+        int result = 0;
+        for(int i = 0; i < 32; i++){
+            result = (result << 1) + (n & 1);
+            n >>= 1;
+        }
+        return result;
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
